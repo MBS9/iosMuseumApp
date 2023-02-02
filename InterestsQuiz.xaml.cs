c7b540ca06014f17dab0fb8e8bc87cadf1b2e5dc
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-02-02 17:34:34 +0100

    feat: personalization

--- InterestsQuiz.xaml.cs
+++ InterestsQuiz.xaml.cs
@@ -7,8 +7,28 @@ public partial class InterestsQuiz : ContentPage
 		InitializeComponent();
 	}
 
+	RadioButton colorBtn;
+	bool selected = false;
+
+	private void checkChange(Object sender, System.EventArgs e)
+	{
+		RadioButton button= sender as RadioButton;
+		if (button.IsChecked && button.GroupName == "colors")
+		{
+			colorBtn = button;
+			selected = true;
+		}
+	}
+
 	public void submitQuiz(object sender, EventArgs e)
     {
-		successLabel.Text = "Submitted!";
+		if (selected)
+		{
+			successLabel.Text = "Submitted!";
+			Preferences.Set("favColor", colorBtn.ContentAsString());
+		} else
+		{
+            successLabel.Text = "Fail!";
+        }
 	}
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        var result = Preferences.Get("favColor", "fill out the Interests Quiz so we know");
+		personalizedGreeting.Text = "Your best prefered colour is " + result + "!";
+    }
 }
-
