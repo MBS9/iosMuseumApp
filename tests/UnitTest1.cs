cb74b18abb21faae4b904f5b903ad4a2df475187
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-04-16 20:00:06 +0200

    try tis fix

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -8,13 +8,20 @@ namespace tests
         /*
         IApp app = ConfigureApp
             .Android
-            .ApkFile("/path/to/android.apk")
+            .ApkFile("../bin/Debug/net7.0-android/com.companyname.museumAppBud.apk")
             .StartApp();
         */
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(true);
+            IApp app = ConfigureApp
+                .Android
+                .ApkFile("../bin/Debug/net7.0-android/com.companyname.museumAppBud-Signed.apk")
+                .StartApp();
+            app.Tap(c => c.Text("National Gallery"));
+            app.Tap(c => c.Text("Expand"));
+            var restults = app.Query(c => c.Text("National Gallery"));
+            Assert.IsTrue(restults.Length > 0);
         }
     }
 }
