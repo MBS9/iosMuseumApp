05f668bbfbcd6b2c4e92e66e18fa23d8296c18ca
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-04-17 21:44:15 +0200

    remove tests as they aren't worth the energy

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,14 +9,4 @@ public partial class App : Application
 		Current.UserAppTheme = AppTheme.Light;
 		MainPage = new AppShell();
     }
-	protected override void OnStart()
-	{
-#if DEBUG
-        Console.WriteLine("Running tests");
-        Preferences.Set(shared.pageSymbol, "museum");
-        Shell.Current.GoToAsync("exampleMuseum");
-        Shell.Current.GoToAsync("..");
-        base.OnStart();
-#endif
-    }
 }
