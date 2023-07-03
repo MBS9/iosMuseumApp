cc5a7ab8a6ec275057691b705d333395df553409
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-07-03 12:01:25 +0200

    fix: improve the efficiency of the code

--- Museum.xaml.cs
+++ Museum.xaml.cs
@@ -4,12 +4,12 @@ namespace museumApp;
 
 public partial class Museum : ContentPage
 {
-    private const int DEFAULT_FONT_SIZE = 18;
+    private const int DEFAULT_TEXT_SIZE = 18;
     readonly Shared shared = new();
 	public Museum()
 	{
 		InitializeComponent();
-        _ = LoadAsset();
+        LoadAsset();
 	}
 
     private void moreDetailsClicked(object sender, EventArgs e)
@@ -18,42 +18,45 @@ public partial class Museum : ContentPage
 		details.IsVisible = true;
     }
 
-    private void outputFromMd(string md, VerticalStackLayout viewParent)
+    private static void outputFromMd(string md, VerticalStackLayout viewParent)
     {
         foreach (var paragraph in md.Split("\n\n")) {
             Element element;
             var content = paragraph.Trim();
             content = content.Trim('\n');
-            if (content.StartsWith('#'))
+            if (content.Length < 2) continue;
+            switch (content[0])
             {
-                var fontSize = 30;
-                var heading =SemanticHeadingLevel.Level2;
-                if (content.StartsWith("##"))
-                {
-                    fontSize = 25;
-                    heading = SemanticHeadingLevel.Level3;
-                }
-                element = new Label { Text = content.Trim('#'), FontSize=fontSize };
-                SemanticProperties.SetHeadingLevel(element, heading);
-            } else if (content.StartsWith('!'))
-            {
-                element = new VerticalStackLayout();
-                var child = new HorizontalStackLayout();
-                string[] imageStrings = content.TrimStart('!').Split(':');
-                Image image = new() { Source = imageStrings[0] };
-                SemanticProperties.SetDescription(image, imageStrings[1]);
-                child.Add(image);
-                (element as VerticalStackLayout).Add(child);
-                (element as VerticalStackLayout).Add(new Label {Text= imageStrings[1], FontSize= DEFAULT_FONT_SIZE });
-            } else 
-            {
-                element = new Label { Text = content, FontSize=DEFAULT_FONT_SIZE };
+                case '#':
+                    var fontSize = 30;
+                    var heading =SemanticHeadingLevel.Level2;
+                    if (content[1] == '#')
+                    {
+                        fontSize = 25;
+                        heading = SemanticHeadingLevel.Level3;
+                    }
+                    element = new Label { Text = content.Trim('#'), FontSize=fontSize };
+                    SemanticProperties.SetHeadingLevel(element, heading);
+                    break;
+                case '!':
+                    element = new VerticalStackLayout();
+                    var child = new HorizontalStackLayout();
+                    string[] imageStrings = content.TrimStart('!').Split(':');
+                    Image image = new() { Source = imageStrings[0] };
+                    SemanticProperties.SetDescription(image, imageStrings[1]);
+                    child.Add(image);
+                    (element as VerticalStackLayout).Add(child);
+                    (element as VerticalStackLayout).Add(new Label {Text= imageStrings[1], FontSize= DEFAULT_TEXT_SIZE });
+                    break;
+                default:
+                    element = new Label { Text = content, FontSize=DEFAULT_TEXT_SIZE };
+                    break;
             }
             viewParent.Add((IView)element);
         }
     }
 
-    async Task LoadAsset()
+    async void LoadAsset()
     {
         var page = Preferences.Get(shared.pageSymbol, "example_museum");
         Preferences.Remove(shared.pageSymbol);
